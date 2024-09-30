35bf59dd0b6f31d8ffb71af52b2dbf2a388e52e7
Tóth László <lastoth@gmail.com>
2024-09-30 11:29:41 +0200

    Add project files.

---
+++ GAMF_ENG/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GAMF_ENG.Models
+{
+    public class Course
+    {
+        //we don't want to generate the CourseId automatically
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] 
+        public  int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
