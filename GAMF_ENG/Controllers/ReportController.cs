ec67b0397e18ee6bff9ff37cf3cbb490ec256e9a
Tóth László <lastoth@gmail.com>
2024-10-14 11:30:41 +0200

    !!!. exercises

---
+++ GAMF_ENG/Controllers/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using GAMF_ENG.Data;
+using GAMF_ENG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GAMF_ENG.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly GAMFDbContext _context;
+
+        public ReportController (GAMFDbContext context)
+        {
+            _context = context 
+                ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IActionResult EnrollmentDateReport()
+        {
+            var result = _context.Students.GroupBy(s => s.EnrollmentDate).
+                Select(s => new EnrollmentDateVM
+                {
+                    EnrollmentDate = s.Key,
+                    StudentCount = s.Count()
+                });
+            
+            /* SQL ->
+             select EnrollmentDate, count(*) from Students
+             group by EnrollmentDate
+             */
+
+            return View(result.ToList());
+        }
+    }
+}
---
+++ GAMF_ENG/Models/EnrollmentDateVM.cs
@@ -0,0 +1,8 @@
+﻿namespace GAMF_ENG.Models
+{
+    public class EnrollmentDateVM
+    {
+        public DateTimeOffset EnrollmentDate { get; set; }
+        public int StudentCount { get; set; }
+    }
+}
