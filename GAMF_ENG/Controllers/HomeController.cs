9eb2996912e71d85667c38212a11682fcb0c777c
Tóth László <lastoth@gmail.com>
2024-10-14 10:35:23 +0200

    II. exercises

--- GAMF_ENG/Controllers/HomeController.cs
+++ GAMF_ENG/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using GAMF_ENG.Data;
 using GAMF_ENG.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -6,27 +7,18 @@ namespace GAMF_ENG.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly GAMFDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(GAMFDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var _student = _context.Students.ToList();
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- GAMF_ENG/Models/Student.cs
+++ GAMF_ENG/Models/Student.cs
@@ -1,12 +1,18 @@
-﻿namespace GAMF_ENG.Models
+﻿using System.ComponentModel;
+
+namespace GAMF_ENG.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+        [DisplayName("First or Midle Name")]
         public string FirstMidName { get; set; }
+        [DisplayName("First Attend")]
         public DateTimeOffset EnrollmentDate { get; set; }
 
+        [DisplayName("Attends")]
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
